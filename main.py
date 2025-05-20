b9c82d30e862f0c33d595f027882744e7e0e7645
bimbims125 <pramvendread@gmail.com>
2025-05-20 21:58:57 +0700

    feat: mango prediction

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ app.add_middleware(
 )
 # Load model sekali saja (lebih efisien)
 model = tf.keras.models.load_model("truck_classifier_best_model.h5")
+model_mangga = tf.keras.models.load_model("model_mangga.h5")
+model_mangga.trainable = False
+
+# Label for mango classification
+classes = ['Bukan Mangga','Terlalu Matang', 'Matang', 'Belum Matang']
 
 def predict_image_from_bytes(image_bytes):
     img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
@@ -31,6 +36,19 @@ def predict_image_from_bytes(image_bytes):
     confidence = float(pred if pred >= 0.5 else 1 - pred)
     return label, confidence
 
+def predict_mango_from_bytes(image_bytes):
+    img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    img = img.resize((64, 64))  # Sesuai ekspektasi model
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)  # Jadi shape (1, 64, 64, 3)
+
+    prediction = model_mangga.predict(img_array)
+    pred_index = np.argmax(prediction)
+    pred_label = classes[pred_index]
+    confidence = float(np.max(prediction))
+    print(prediction)
+    return pred_label, confidence
+
 @app.post("/truck-recognition")
 async def truck_recognition(file: UploadFile = File(...)):
     image_bytes = await file.read()
@@ -43,3 +61,13 @@ async def truck_recognition(file: UploadFile = File(...)):
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Truck Recognition API!"}
+
+@app.post("/mango-prediction")
+async def mango_prediction(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+
+    label, confidence = predict_mango_from_bytes(image_bytes)
+    return JSONResponse(content={
+        "prediction": label,
+        "confidence": f"{confidence:.2%}",
+    })
